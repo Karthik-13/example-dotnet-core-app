e1bbcbc3897f27c36bf78cd5fbc21f97f072efc3
Karthick Prabu Ponnusamy <Karthick.Ponnusamy@viacomcontractor.com>
2019-01-30 12:15:19 +0530

    Added sample dotnet core app

---
+++ example-dotnet-core-canary-app/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using example_dotnet_core_canary_app.Models;
+
+namespace example_dotnet_core_canary_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "This is a Canary .NET Core application.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "This page contains the contact information";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
