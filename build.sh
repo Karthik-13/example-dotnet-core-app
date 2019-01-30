3e91146a30c224d2969bf4a4b48697f5326c73aa
Karthick Prabu Ponnusamy <Karthick.Ponnusamy@viacomcontractor.com>
2019-01-30 15:35:16 +0530

    Added pipeline steps

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ set -e -o pipefail
 
 cd ${WORKSPACE}
 
+export PATH=${PATH}:${HOME}/.dotnet/tools
+
 dotnet restore
 dotnet sonarscanner begin /k:"org.sonarqube:example-dotnet-canary-app" /n:"Example of SonarQube Scanner for MSBuild Usage" /v:"1.0"
 dotnet build
